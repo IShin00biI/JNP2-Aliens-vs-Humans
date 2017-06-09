f85ccc6aa972f78fa09bfc642d4ad9c56f1e31a2
Michał Naruniec <mn360386@students.mimuw.edu.pl>
2017-06-09 21:20:05 +0200

    Copied index.android to index.ios

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,90 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+  AppRegistry, View, Text,
+  Button, Alert
+} from 'react-native'
+import {
+  styles
+} from './styles/styles'
+import {
+  intervalLength, Timer
+} from './components/timer'
+import {
+  Hole, human
+} from './components/hole'
 
-export default class zadanie2 extends Component {
-  render() {
+class zadanie2 extends Component {
+  constructor (props) {
+    super(props)
+    this.holesNumber = 4
+    this.state = this.initialState()
+  }
+
+  initialState () {
+    return { aliensKilled: 0, humansKilled: 0, gameStarted: false }
+  }
+
+  componentDidMount = () => {
+    if (!this.state.gameStarted) {
+      this.intervalObj = setInterval(this.refs.timer.decreaseTimer, intervalLength)
+      const newState = Object.assign({}, this.state)
+      newState.gameStarted = true
+      this.setState(newState)
+    }
+  }
+
+  componentDidUpdate = this.componentDidMount
+
+  render () {
+    let holes = []
+    for (let i = 0; i < this.holesNumber; i++) {
+      holes.push(<Hole key={i} ref={'' + i} onKill={this.onKill} />)
+    }
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View>
+        <Timer ref='timer' onTimeEnd={this.onTimeEnd} />
+        <View style={styles.holeContainer} >
+          {holes}
+        </View>
+        <Text style={styles.label} > Humans killed: {this.state.humansKilled} </Text>
+        <Text style={styles.label} > Aliens killed: {this.state.aliensKilled} </Text>
+        <Button title='RESPAWN' onPress={this.resetHoles} />
       </View>
-    );
+    )
+  }
+
+  resetHoles = () => {
+    for (let i = 0; i < this.holesNumber; i++) {
+      this.refs[i].resetHole()
+    }
+  }
+
+  onKill = (unit) => {
+    const newState = Object.assign({}, this.state)
+    if (unit === human) newState.humansKilled++
+    else newState.aliensKilled++
+    this.setState(newState)
+  }
+
+  onTimeEnd = () => {
+    clearInterval(this.intervalObj)
+    Alert.alert(
+      'Time out!',
+      "You've run out of time, start over\n\n" +
+      this.state.humansKilled + ' humans killed\n' +
+      this.state.aliensKilled + ' aliens killed',
+      [ { text: 'RESET', onPress: this.resetGame },
+      { text: 'debugCANCEL', onPress: () => {} } ],
+      { cancelable: false }
+    )
+  }
+
+  resetGame = () => {
+    this.resetHoles()
+    this.refs.timer.resetTimer()
+    this.setState(this.initialState())
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('zadanie2', () => zadanie2);
+AppRegistry.registerComponent('zadanie2', () => zadanie2)
